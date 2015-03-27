537a21eaffe2402ec43e1db882f599ce65436c28
John Thompson <john@springframework.guru>
2015-03-27 06:55:04 -0400

    adding spring project files

---
+++ src/main/java/di-example/DiExampleApplication.java
@@ -0,0 +1,12 @@
+package di-example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiExampleApplication.class, args);
+    }
+}
---
+++ src/test/java/di-example/DiExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package di-example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DiExampleApplication.class)
+public class DiExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
